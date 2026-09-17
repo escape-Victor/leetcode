c6009000eda1d305c439fa6757ad9121ee0214f5
fanyi <fanyiyang0427@outlook.com>
2026-09-17 18:49:30 +0800

    retry pending LC Buddy progress

---
+++ solutions/fyy/242-valid-anagram.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        temp = {}
+        for i in s:
+            if i in temp:
+                temp[i] += 1
+            else:
+                temp[i] = 1
+        for j in t:
+            if j in temp:
+                temp[j] -= 1
+                if temp[j] == 0:
+                    del temp[j]
+            else:
+                return False
+        if len(temp) == 0:
+            return True
+        else:
+            return False
+    
+# @lc code=end
+
---
+++ solutions/fyy/49-group-anagrams.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=49 lang=python3
+#
+# [49] Group Anagrams
+#
+
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        
+# @lc code=end
+
