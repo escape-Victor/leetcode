ad2662ab526deb3c7dd261d405a839cc59906295
fanyi <fanyiyang0427@outlook.com>
2026-09-19 12:46:29 +0800

    AC progress: fyy

---
+++ solutions/fyy/347-top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=347 lang=python3
+#
+# [347] Top K Frequent Elements
+#
+
+# @lc code=start
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        
+# @lc code=end
+
--- solutions/fyy/36-valid-sudoku.py
+++ solutions/fyy/36-valid-sudoku.py
@@ -7,6 +7,20 @@
 # @lc code=start
 class Solution:
     def isValidSudoku(self, board: list[list[str]]) -> bool:
-        
+        rows = [set() for i in range(9)]
+        cols = [set() for i in range(9)]
+        boxes = [set() for i in range(9)]
+        for r in range(9):
+            for c in range(9):
+                num = board[r][c]
+                if num == '.':
+                    continue
+                box = (r // 3) * 3 + (c // 3)
+                if num in rows[r] or num in cols[c] or num in boxes[box]:
+                    return False
+                rows[r].add(num)
+                cols[c].add(num)
+                boxes[box].add(num)
+        return True
 # @lc code=end
 
